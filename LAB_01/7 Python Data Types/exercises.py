f86efd6cb89c6b648415136f0e16dce58896b5cf
Ali-060 <kailarbekali06@gmail.com>
2024-01-24 19:27:06 +0600

    LAB_01

---
+++ LAB_01/7 Python Data Types/exercises.py
@@ -0,0 +1,40 @@
+#1
+x = 5
+print(type(x))
+#Answer: int
+
+
+#2
+x = "Hello World"
+print(type(x))
+#Answer: str
+
+
+#3
+x = 20.5
+print(type(x))
+#Answer: float
+
+
+#4
+x = ["apple", "banana", "cherry"]
+print(type(x))
+#Answer: list
+
+
+#5
+x = ("apple", "banana", "cherry")
+print(type(x))
+#Answer: tuple
+
+
+#6
+x = {"name" : "John", "age" : 36}
+print(type(x))
+#Answer: dict
+
+
+#7
+x = True
+print(type(x))
+#Answer: bool
